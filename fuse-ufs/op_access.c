3ffab5eceb40c70eba005e33193b8072516b2988
Jan Blumschein <jan@jan-blumschein.de>
2016-04-27 12:56:21 +0200

    (#2, #6) Fix fuse-ufs/op_access.c: make sure that config.h is included first
    config.h defines _FILE_OFFSET_BITS to control size of ino_t etc. in sys headers

--- fuse-ufs/op_access.c
+++ fuse-ufs/op_access.c
@@ -18,10 +18,10 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include "fuse-ufs.h"
+
 #include <unistd.h>
 #include <sys/stat.h>
-
-#include "fuse-ufs.h"
 
 
 static int inode_access(const struct inode *inode, int mode)
